5b3c5fea149e54e9a7f7ed0b6e07d354f67185eb
Matt Williams <matt.seongjun@gmail.com>
2019-05-27 12:37:56 -0400

    Add db connection

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,11 @@
 require 'bundler'
 Bundler.require
+require 'active_record'
 
 # Setup a DB connection here
+
+
+DB = ActiveRecord::Base.establish_connection({
+  adapter: 'sqlite3',
+  database: 'db/guests.db'
+})
